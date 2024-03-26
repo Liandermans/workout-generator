d22ac69475901a05a93af6fe04af4b5df78fb081
Liandermans <139888264+Liandermans@users.noreply.github.com>
2024-03-26 17:19:24 +0100

    Optimized AI context

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ function generateWorkout(event) {
   event.preventDefault();
 
   new Typewriter("#generated-workout", {
-    strings: `Generating your workout for today. Please wait...`,
+    strings: `<strong>Generating your workout for today. Please wait...</strong>`,
     autoStart: true,
     delay: 50,
     cursor: null,
@@ -11,8 +11,8 @@ function generateWorkout(event) {
   const generatedWorkout = document.getElementById("generated-workout");
   const bodyPart = document.getElementById("body-part");
   const apiKey = "9fb66eat3c45068of64821d7cabe200f";
-  let context = `Please make sure to provide your answer in basic HTML-format. Please provide 5 exercises first. Then provide instructions with the number of sets and number of repetitions per exercise.`;
-  let prompt = `Please generate a workout for the ${bodyPart}.`;
+  let context = `You are a fitness instruction AI Assistent. Please make sure to provide your answer in basic HTML-format. Start your answer with the h2 element "Workout Plan". Then provide an ordered list containing 5 exercises under the h3 element "Excersises:". Then provide a p element containing instructions with the number of sets and the number of repetitions for each exercise under the h3 element "Instructions:".`;
+  let prompt = `Please generate a ${bodyPart.value} workout.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   function showWorkout(response) {
