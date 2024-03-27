7266e62734031b5f629fbb35e18fc0aa10471180
Liandermans <139888264+Liandermans@users.noreply.github.com>
2024-03-27 17:27:55 +0100

    Worked on UX

--- src/script.js
+++ src/script.js
@@ -8,18 +8,22 @@ function generateWorkout(event) {
     cursor: null,
   });
 
-  const generatedWorkout = document.getElementById("generated-workout");
-  const bodyPart = document.getElementById("body-part");
-  const apiKey = "9fb66eat3c45068of64821d7cabe200f";
-  let context = `You are a fitness instruction AI Assistent. Please make sure to provide your answer in basic HTML-format. Start your answer with the h2 element "Workout Plan". Then provide an ordered list containing 5 exercises under the h3 element "Excersises:". Then provide a p element containing instructions with the number of sets and the number of repetitions for each exercise under the h3 element "Instructions:".`;
-  let prompt = `Please generate a ${bodyPart.value} workout.`;
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  function delayFunction() {
+    const generatedWorkout = document.getElementById("generated-workout");
+    const bodyPart = document.getElementById("body-part");
+    const apiKey = "9fb66eat3c45068of64821d7cabe200f";
+    let context = `You are a fitness instruction AI Assistent. Please make sure to provide your answer in basic HTML-format. Start your answer with the h2 element "Workout Plan". Then provide an ordered list containing 5 exercises under the h3 element "Excersises:". Then provide a p element containing instructions with the number of sets and the number of repetitions for each exercise under the h3 element "Instructions:".`;
+    let prompt = `Please generate a ${bodyPart.value} workout.`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  function showWorkout(response) {
-    generatedWorkout.innerHTML = response.data.answer;
+    function showWorkout(response) {
+      generatedWorkout.innerHTML = response.data.answer;
+    }
+
+    axios.get(apiUrl).then(showWorkout);
   }
 
-  axios.get(apiUrl).then(showWorkout);
+  setTimeout(delayFunction, 2000);
 }
 
 let formElement = document.querySelector("form");
